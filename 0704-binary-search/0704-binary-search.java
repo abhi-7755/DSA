8caa8e816773cbec6115ce6e99cce41ede131d1c
Abhishek Gaikwad <abhishekgaikwad6567@gmail.com>
2026-09-12 23:32:34 +0530

    Time: 0 ms (100%), Space: 48.4 MB (49.57%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int search(int[] arr, int tar) {
+        
+        
+        int lo = 0, hi = arr.length - 1;
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;  
+            
+            if (arr[mid] < tar) {
+                lo = mid + 1;
+            } 
+            else if (arr[mid] > tar) {
+                hi = mid - 1;
+            } 
+            else {
+                return mid;
+                
+
+            }
+        }
+        
+        return -1;   
+    }
+}
